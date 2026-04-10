f60963748f8bd0d38d08a0c0ce69255fcc58bfd6
abdurehman-26 <ar2522006@gmail.com>
2026-04-10 07:59:33 +0500

    update coin type interface

--- types/CoinData.ts
+++ types/CoinData.ts
@@ -1,32 +1,12 @@
-export type CoinData = {
-  id: string;
-  symbol: string;
+export interface CoinData {
+  rank: string;
   name: string;
+  symbol: string;
   image: string;
-  current_price: number;
-  market_cap: number;
-  market_cap_rank: number;
-  fully_diluted_valuation: number | null;
-  total_volume: number;
-  high_24h: number;
-  low_24h: number;
+  price: number;
+  price_change_1h: number;
   price_change_24h: number;
-  price_change_percentage_24h: number;
-  market_cap_change_24h: number;
-  market_cap_change_percentage_24h: number;
-  circulating_supply: number;
-  total_supply: number | null;
-  max_supply: number | null;
-  ath: number;
-  ath_change_percentage: number;
-  ath_date: string; // ISO date string
-  atl: number;
-  atl_change_percentage: number;
-  atl_date: string; // ISO date string
-  roi?: {
-    times: number;
-    currency: string;
-    percentage: number;
-  } | null;
-  last_updated: string; // ISO date string
-};
+  price_change_7d: number;
+  volume_24h: number;
+  market_cap: number;
+}
