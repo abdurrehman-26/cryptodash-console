60e45f089fdb25b7ce1cbdbdfdfbf661deb36222
abdurehman-26 <ar2522006@gmail.com>
2026-04-10 08:34:27 +0500

    change live updates to price updates

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default async function Home() {
               <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
               <span className="relative inline-flex rounded-full h-2 w-2 bg-green-500"></span>
             </span>
-            <span className="text-sm font-medium text-slate-600">Live Updates</span>
+            <span className="text-sm font-medium text-slate-600">Price Updates</span>
           </div>
         </div>
       </header>
