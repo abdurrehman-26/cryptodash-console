76b521e7c147c8411e6ce656300bee6bf9c868e1
abdurehman-26 <ar2522006@gmail.com>
2026-04-10 07:59:18 +0500

    update the image hostname

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'coin-images.coingecko.com',
+        hostname: 'assets.coingecko.com',
       },
     ],
   },
